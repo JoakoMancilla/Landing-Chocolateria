e30c86681d8d4b30112dd799c47d8c82eac4465a
JoakoMancillaDev <JoakoMancillaVr@gmail.com>
2025-12-30 00:07:15 -0300

    fix: mejoras en el scroll al cambiar de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { ScrollToTop } from './componentes/ScrollTop'
 import Inicio from './pages/Inicio'
 import Ventas from './pages/Ventas'
 import { Routes, Route } from 'react-router-dom'
@@ -7,6 +8,9 @@ import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <>
+    
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/Ventas" element={<Ventas />} />
