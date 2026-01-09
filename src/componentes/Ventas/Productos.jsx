edf069b8772b3c82a5c50ad4676037b7f4d1e2e8
JoakoMancillaDev <JoakoMancillaVr@gmail.com>
2026-01-09 01:25:55 -0300

    fix: arreglos en cards de la pag PRODUCTOS

--- src/componentes/Ventas/Productos.jsx
+++ src/componentes/Ventas/Productos.jsx
@@ -1,31 +1,53 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Productos = () => {
+  const [data, setData] = useState([])
 
-  const [data, setData] = useState(null);
-
-  useEffect(() =>{
+  useEffect(() => {
     fetch('https://fakestoreapi.com/products/')
-            .then(res=>res.json())            
-            .then(data=>setData(data))
-  },[]);
-
-
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
 
   return (
-    <div className='bg-green-50 pt-20'>
-      <h1 className='text-2xl p-4 px-0'>
-        Productos:
-      </h1>
-        <ol>
-          {data?.map((products) => (
-            <li key={products.id} className='bg-emerald-100 p-2 m-2 rounded-2xl w-2xs h-40 float-left'>
-              <img src={products.image} className='h-10 w-10 self-center'/>
-              <h3>{products.title}</h3>
-              <p>Precio: ${products.price}</p>
-            </li>
-          ))}
-        </ol>
+    <div className="bg-green-50 pt-20 px-4 sm:px-8 sm:pb-8 p-4">
+      <h1 className="text-2xl mb-6">Productos</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-6 gap-6">
+        
+        {/* Filtros */}
+        <aside className="md:col-span-1 bg-white h-fit p-4 rounded-2xl shadow">
+          <h2 className="font-semibold mb-2">Filtros</h2>
+        </aside>
+
+        {/* Productos */}
+        <section className="md:col-span-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {data.map(product => (
+              <div
+                key={product.id}
+                className="bg-white p-4 rounded-2xl shadow hover:shadow-lg transition "
+              >
+                <div className='bg-gray-100 rounded-xl py-2 mb-2'>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="h-32 object-contain mx-auto mb-4 "
+                  />
+                </div>
+
+                <h3 className="text-sm font-medium line-clamp-2">
+                  {product.title}
+                </h3>
+
+                <p className="text-lg text-emerald-700 font-semibold mt-4">
+                  ${product.price}
+                </p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
-  );
+  )
 }
