0886d43d3a5b14819c14d8c09ca3524db462beba
JoakoMancillaDev <JoakoMancillaVr@gmail.com>
2026-01-10 01:20:02 -0300

    fix: Agregar checkbox en seccion de filtros (aun no son funcionales)

--- src/componentes/Ventas/Productos.jsx
+++ src/componentes/Ventas/Productos.jsx
@@ -14,11 +14,36 @@ export const Productos = () => {
       <h1 className="text-2xl mb-6">Productos</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-6 gap-6">
-        
+
         {/* Filtros */}
         <aside className="md:col-span-1 bg-white h-fit p-4 rounded-2xl shadow">
-          <h2 className="font-semibold mb-2">Filtros</h2>
+          <h2 className="font-semibold mb-4">Filtros</h2>
+
+          <fieldset className="space-y-2">
+            <legend className="sr-only">Tipo de chocolate</legend>
+
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                name="tipo"
+                value="Chocolate Negro"
+                className="accent-amber-600"
+              />
+              Chocolate Negro
+            </label>
+
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                name="tipo"
+                value="Chocolate Blanco"
+                className="accent-amber-600"
+              />
+              Chocolate Blanco
+            </label>
+          </fieldset>
         </aside>
+
 
         {/* Productos */}
         <section className="md:col-span-5">
@@ -29,10 +54,10 @@ export const Productos = () => {
                 className="bg-white p-4 rounded-2xl shadow hover:shadow-lg transition "
               >
                 <div className='bg-gray-100 rounded-xl py-2 mb-2'>
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="h-32 object-contain mx-auto mb-4 "
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="h-32 object-contain mx-auto mb-4 "
                   />
                 </div>
 
